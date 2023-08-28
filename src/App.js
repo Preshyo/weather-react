00e2f4b900af5036c670f01e9c8324985907f65a
Preshyo <66226812+Preshyo@users.noreply.github.com>
2023-08-28 19:40:24 +0100

    Added my GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <footer> This project was coded by Precious Ohasonu and is {''}
+        <a href='https://github.com/Preshyo/weather-react' target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
